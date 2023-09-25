488ea99404f1a7ce86c63dfc5de92fb8fb43e839
avsholeh <avsholeh@gmail.com>
2023-09-25 08:24:14 +0700

    buat route departemen

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DepartemenController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,4 +11,5 @@ Auth::routes();
 
 Route::middleware('auth')->group(function() {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('departemen', DepartemenController::class);
 });
