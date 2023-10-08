933d5739ad6e2cd82bd5d02aa484ed3f42eb2ae3
avsholeh <avsholeh@gmail.com>
2023-10-09 00:18:33 +0700

    tambah route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DepartemenController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,4 +11,10 @@ Auth::routes();
 
 Route::middleware('auth')->group(function() {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('/departemen', DepartemenController::class)->names([
+        'index' => 'departemen.index',
+        'store' => 'departemen.store',
+        'edit' => 'departemen.edit',
+        'update' => 'departemen.update',
+    ]);
 });
